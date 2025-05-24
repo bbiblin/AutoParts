f6805d40ab10aed761675723b457a9b633e17c08
Alan Gornall <ltpalangornall@gmail.com>
2025-05-24 00:11:49 -0400

    arreglo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 import Productos from './pages/productos'
+import './index.css'
+import './navbar.css'
+
 
 
 export default function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import Navbar from './components/navbar.jsx'
+import './index.css'
+import './navbar.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
