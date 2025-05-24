f95f7593e27d3cc230f5dd198c104f93da5e7eaf
belen <bele.diaz@duocuc.cl>
2025-05-23 22:43:06 -0400

    ordenado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Asegúrate de que la raíz sea correcta
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 })
