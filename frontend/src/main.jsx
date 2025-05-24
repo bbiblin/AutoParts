a64dbd99f6af28a7c4ce608d528303f330d68fbb
belen <bele.diaz@duocuc.cl>
2025-05-23 21:05:12 -0400

    arreglo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './app.css'
 import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <BrowserRouter>
-    <Navbar />
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
