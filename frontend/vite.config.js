f614a0f14a05e770bcca04f82c6d6df7ef16862c
belen <bele.diaz@duocuc.cl>
2025-05-23 22:43:27 -0400

    ordenado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Asegúrate de que la raíz sea correcta
+  root: '.',
   build: {
     outDir: 'dist',
     sourcemap: false,
