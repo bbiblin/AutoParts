c55269d9a40f15ed3508cafb0ad62a5a8999f1f4
belen <bele.diaz@duocuc.cl>
2025-05-27 22:51:38 -0400

    arreglos login

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import Productos from './pages/productos'
 import '../src/index.css'
 import Login from './pages/login'
 import Register from './pages/register'
-import { AuthProvider } from './contexts/authContext'
-
-
+import { AuthProvider } from './contexts/authContext' // asegúrate de usar la capitalización correcta
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/productos" element={<Productos />} />
-        <Route path="/users/login" element={<Login />} />
-        <Route path="/users/register" element={<Register />} />
-      </Routes>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/users/login" element={<Login />} />
+          <Route path="/users/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
