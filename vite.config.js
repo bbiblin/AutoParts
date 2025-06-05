a2020fbbd23bc6ac9da90c4072abec748cdb144a
Alan Gornall <130397986+Washaaaan@users.noreply.github.com>
2025-06-05 06:18:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
